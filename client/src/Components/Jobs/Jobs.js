a1cb53f5f90935e9ac64d49a6b4ae0e41538368d
AbdalQader Baghi <abdalqaderbaghi@gmail.com>
2020-07-17 01:26:13 +0200

    rendered the jobs from the database into the UI, next up, user auth

--- client/src/Components/Jobs/Jobs.js
+++ client/src/Components/Jobs/Jobs.js
@@ -3,18 +3,38 @@ import SearchBar from './SearchBar.js';
 import './jobs.css';
 
 class Jobs extends Component {
+  state = {
+    jobPosts: [],
+  };
+
+  getJobPosts = async () => {
+    const jobsPromise = await fetch('/api/job/all-job?limit=10&skip=0?');
+    const jobsJSON = await jobsPromise.json();
+    this.setState({
+      jobPosts: [...jobsJSON],
+    });
+  };
+
+  componentDidMount() {
+    this.getJobPosts();
+  }
+
   JobDashBaurd = () => {
     return (
       <div className="jobs-dashbaurd">
         <div className="previous-searches">previous searches</div>
         <div className="job-postings">
-          <div className="job-post">
-            <h1 className="job-title">Web Developer</h1>
-            <p className="company-name">Amazon</p>
-            <p className="location">Manchester</p>
-            <p className="salary">35,000£</p>
-            <p className="time-of-post">5 hours ago</p>
-          </div>
+          {this.state.jobPosts.map((jobPost) => {
+            return (
+              <div className="job-post" key={jobPost._id}>
+                <h1 className="job-title">{jobPost.catagory}</h1>
+                <p className="company-name">{jobPost.companyName}</p>
+                <p className="location">{jobPost.location}</p>
+                <p className="salary">{jobPost.salary}</p>
+                <p className="time-of-post">{jobPost.createdAt}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
