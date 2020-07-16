f61d26fa65a94d11a448e03a5a71a7703864dd30
AbdalQader Baghi <abdalqaderbaghi@gmail.com>
2020-07-16 18:08:12 +0200

    modified search bar, next up, structure the front end components and redux store

--- client/src/Components/Jobs/Jobs.js
+++ client/src/Components/Jobs/Jobs.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar.js';
 import './jobs.css';
 
 class Jobs extends Component {
+  JobDashBaurd = () => {
+    return (
+      <div className="jobs-dashbaurd">
+        <div className="previous-searches">previous searches</div>
+        <div className="job-postings">
+          <div className="job-post">
+            <h1 className="job-title">Web Developer</h1>
+            <p className="company-name">Amazon</p>
+            <p className="location">Manchester</p>
+            <p className="salary">35,000£</p>
+            <p className="time-of-post">5 hours ago</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   render() {
     return (
       <div className="jobs no-outline">
-        <div className="search-form">
-          <input typr="text" name="search" autoComplete="off" required />
-          <label htmlFor="search" className="label-search">
-            <span className="content-search">Search</span>
-          </label>
-        </div>
+        <SearchBar />
+        <this.JobDashBaurd />
       </div>
     );
   }
